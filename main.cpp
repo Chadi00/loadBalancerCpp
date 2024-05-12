fcf7e5fafbe172a57990be3cb849684d434d8e5e
Chadi00 <elkhiatichadi@outlook.fr>
2024-05-12 02:27:48 -0400

    make socket non blocking and run concurrent

--- main.cpp
+++ main.cpp
@@ -5,48 +5,73 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <queue>
+#include <condition_variable>
 #include <csignal>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <algorithm>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <atomic>
+#include <functional>
 
 using namespace std;
 
-// Mock backend servers running on the host machine while loadbalancer on docker coontainer
+// Mock backend servers
 const vector<string> servers = { "host.docker.internal:8081", "host.docker.internal:8082", "host.docker.internal:8083" };
 vector<int> active_connections(servers.size(), 0);
 mutex connections_mutex;
 
-// Function to choose servers based on the least connections
-void get_least_connection_server(string& host, string& port) {
-    lock_guard<mutex> lock(connections_mutex);
-    int min_index = 0;
-    for (int i = 1; i < active_connections.size(); ++i) {
-        if (active_connections[i] < active_connections[min_index]) {
-            min_index = i;
-        }
+void make_socket_non_blocking(int sockfd) {
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags == -1) {
+        cerr << "Error getting socket flags\n";
+        return;
     }
-    string server = servers[min_index];
-    auto pos = server.find(':');
+    flags |= O_NONBLOCK;
+    if (fcntl(sockfd, F_SETFL, flags) == -1) {
+        cerr << "Error setting socket to non-blocking\n";
+    }
+}
+
+// Function to get server using round robin method
+void get_round_robin_server(string &host, string &port) {
+    lock_guard<mutex> lock(rr_mutex);
+    string server = servers[rr_index % servers.size()];
+    rr_index = (rr_index + 1) % servers.size(); // Increment index for round robin
+
+    size_t pos = server.find(':');
     if (pos != string::npos) {
         host = server.substr(0, pos);
         port = server.substr(pos + 1);
     } else {
-        host = server;  
-        port = "80";    // Default HTTP port
+        host = server;
+        port = "80"; 
     }
-    active_connections[min_index]++;
 }
 
-// Function to handle each client request in a separate thread
+void worker_thread() {
+    function<void()> task;
+    while (true) {
+        {
+            unique_lock<mutex> lock(queue_mutex);
+            condition.wait(lock, []{ return !tasks.empty() || !server_running; });
+            if (!server_running && tasks.empty()) break;
+            task = tasks.front();
+            tasks.pop();
+        }
+        task(); 
+    }
+}
+
 void handle_client(int client_sockfd) {
     char buffer[4096];
     string host, port;
 
-    get_least_connection_server(host, port);  // Get the server with least connections
+    get_round_robin_server(host, port); // Get the server with round robin ditribution
 
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(hints));
@@ -73,33 +98,24 @@ void handle_client(int client_sockfd) {
         }
     }
 
-    {
-        lock_guard<mutex> lock(connections_mutex);
-        auto pos = find(servers.begin(), servers.end(), host + ":" + port);
-        if (pos != servers.end()) {
-            ptrdiff_t index = distance(servers.begin(), pos);
-            active_connections[index]--;
-        }
-    }
-
     close(server_sockfd);
     close(client_sockfd);
 }
 
-volatile sig_atomic_t keep_running = 1;
-
-void signal_handler(int sig) {
-    keep_running = 0;
+void initialize_thread_pool(size_t num_threads) {
+    for (size_t i = 0; i < num_threads; ++i) {
+        thread(worker_thread).detach();
+    }
 }
 
-
 int main() {
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    signal(SIGINT, [](int){ server_running = false; condition.notify_all(); });
+    signal(SIGTERM, [](int){ server_running = false; condition.notify_all(); });
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_addr;
+    make_socket_non_blocking(sockfd);  // Make the socket non blocking
 
+    struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
@@ -108,16 +124,40 @@ int main() {
     bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     listen(sockfd, 10);
 
+    fd_set master_set;
+    FD_ZERO(&master_set);
+    FD_SET(sockfd, &master_set);
+    int max_sd = sockfd;
+
     cout << "Load Balancer running on port 8080" << endl;
 
-    while (keep_running) {
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
-        if (client_sockfd < 0) continue;
+    initialize_thread_pool(10);
 
-        thread t(handle_client, client_sockfd);
-        t.detach();
+    while (server_running) {
+        fd_set read_fds = master_set;
+        if (select(max_sd + 1, &read_fds, NULL, NULL, NULL) < 0) {
+            cerr << "Select error." << endl;
+            continue;
+        }
+
+        if (FD_ISSET(sockfd, &read_fds)) {
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+            int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+            if (client_sockfd < 0) {
+                if (errno != EWOULDBLOCK) {
+                    cerr << "Accept failed." << endl;
+                    continue;
+                }
+            } else {
+                make_socket_non_blocking(client_sockfd);  // Make client socket non blocking
+                {
+                    lock_guard<mutex> lock(queue_mutex);
+                    tasks.emplace([client_sockfd]{ handle_client(client_sockfd); });
+                    condition.notify_one();
+                }
+            }
+        }
     }
 
     close(sockfd);
